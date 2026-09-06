618845b9b3c47b2f7d57122215a4a79ce6cfbdc6
PurvaGangrade <85606360+PurvaGangrade@users.noreply.github.com>
2026-09-06 19:50:19 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "devops-project-terraform-state-purva"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
